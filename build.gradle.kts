67d33a6d74b79b952787536de60aed55e7d5b2e9
Oak <55620516+OakLoaf@users.noreply.github.com>
2025-05-13 00:29:56 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
     registerFeature("optional") {
         usingSourceSet(sourceSets["main"])
