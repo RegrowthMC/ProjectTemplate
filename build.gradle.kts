218dc5264e12af3f3776c356db9d7cf443f7ff91
Oak <55620516+OakLoaf@users.noreply.github.com>
2026-01-27 12:34:01 +0000

    Update plugin versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("com.gradleup.shadow") version("8.3.0")
-    id("xyz.jpenilla.run-paper") version("2.2.4")
+    id("com.gradleup.shadow") version("9.3.1")
+    id("xyz.jpenilla.run-paper") version("3.0.2")
 }
 
 group = "org.lushplugins"
